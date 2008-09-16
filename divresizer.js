f2f82f011526c12281638df78fe82f1be0b06396
James R Glasgow <james@jrglasgow.com>
2008-09-16 05:23:08 +0000

    fixed the problem where the variables div was not resizing

--- divresizer.js
+++ divresizer.js
@@ -1,3 +1,4 @@
+// $Id$
 Drupal.contemplate.divResizable = function() {
   $('div.resizable:not(.processed)').each(function() {
     var div = $(this).addClass('processed'), staticOffset = null;
@@ -9,19 +10,19 @@ Drupal.contemplate.divResizable = function() {
     grippie.style.marginRight = (grippie.offsetWidth - $(this)[0].offsetWidth) +'px';
 
     function startDrag(e) {
-      staticOffset = div.height() - Drupal.mousePosition(e).y;
+      staticOffset = div.height() - e.pageY;
       div.css('opacity', 0.25);
       $(document).mousemove(performDrag).mouseup(endDrag);
       return false;
     }
 
     function performDrag(e) {
-      div.height(Math.max(32, staticOffset + Drupal.mousePosition(e).y) + 'px');
+      div.height(Math.max(32, staticOffset + e.pageY) + 'px');
       return false;
     }
 
     function endDrag(e) {
-      $(document).unmousemove(performDrag).unmouseup(endDrag);
+      $(document).unbind("mousemove", performDrag).unbind("mouseup", endDrag);
       div.css('opacity', 1);
     }
   });
